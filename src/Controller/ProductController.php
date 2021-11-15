cb3dc77de4c713c95f41267fd3ea680a6a732708
ajkolodziej <ajkolodziej@gmail.com>
2021-11-15 03:26:45 +0100

    poprawki

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Exception;
 use App\Entity\Product;
 use App\Entity\Category;
+use App\Service\SaveProduct;
 use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -53,76 +54,28 @@ class ProductController extends AbstractController
      * 
      * @throws \Exception
      */
-    public function show(int $id): Response
+    public function show(int $id, SaveProduct $saveProduct, Request $request): Response
     {
         $product = $this->manager->getRepository(Product::class)
                 ->find($id);
         $product->getCategory();
+        
         return $this->render("product/show.html.twig",[
             'product'=> $product
         ]);
-        // return new Response($product);
     }
 
     /**
-     * @Route("/products", name="products_create", methods="POST")
+     * @Route("/api/products", name="products_create", methods="POST")
      *
      * @param Request $request
-     * @return Response
+     * @return JsonResponse
      * @throws \Exception 
      */
-    public function create(Request $request): Response
+    public function create(Request $request, SaveProduct $saveProduct): JsonResponse
     {
-        $categories = $request->get('categories');
-        $message = '';
-        $categoryMsg = ' to the categories: ';
-        
-        if(is_array($categories)){
-            $entityManager = $this->getDoctrine()->getManager();
-            if(!$product = $entityManager
-                ->getRepository(Product::class)
-                ->findOneBy([
-                    'name'=> $request->get('name'),
-                    'price' => $request->get('price')
-                    ])
-                ){
-                    $product = new Product();
-                    $product->setName($request->get('name'));
-                    $product->setPrice($request->get('price'));
-                }
-            
-            for($i = 0; $i < 2; $i++){
-                if(!$category = $entityManager->getRepository(Category::class)->findOneBy(['code'=>$categories[$i]])){
-                    $category = new Category();
-                    $category->setCode($categories[$i]);
-                    $entityManager->persist($category);
-                } 
-                $product->addCategory($category);
-                $categoryMsg .= "<br>".$category->getCode();
-            }
-            $entityManager->persist($product);
-            try{
-                if($entityManager->flush()){
-                    // dump("test");
-                }
-                $message .= "Saved new product:<br>";
-                $message .= "name: ".$product->getName()."<br>";
-                $message .= "price: ".$product->getPrice()."<br>";
-                $message .= "<br>";
-                $message .= $categoryMsg;
-                $notification = (new Notification('New Invoice', ['email']))
-                    ->content('You got a new invoice for 15 EUR.');
-                $this->notifier->send($notification);
-                dump($notification);
-            } 
-            catch(Exception $e){
-                $message = $e->getMessage();
-            }
-            $this->logger->info($message);
-            // dump($logger);
-        }
-        return new Response(
-            "<h1>".$message."</h1>"
-        );
+        $object = $saveProduct->save($request);
+        // $resp = json_decode($object->getContent(), true);
+        return new JsonResponse($object['msg'], $object['code']);
     }
 }
