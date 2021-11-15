049660b2dd8ad5fedb1f9a88e69798c2f08b5dc6
ajkolodziej <ajkolodziej@gmail.com>
2021-11-15 03:44:17 +0100

    poprawki categoryRepositiory()

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,39 +19,6 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    public function findByWithCategory()
-    {
-        $em = $this->getEntityManager();
-        $em->createQueryBuilder()
-        ->select('product.id, product.name, product.price, category.code')
-        ->from('product','p')
-        ->join('category', 'category.id = product_category.category_id')
-        ->getQuery()
-        ->getResult();
-        dump($em);die();
-        // $query = $entityManager->createQuery(
-        //     'SELECT  product.id, product.name, product.price, category.code
-        //     FROM App\Entity\Product
-        //     LEFT JOIN product_category ON product.id=product_category.product_id
-        //     LEFT JOIN category ON category.id=product_category.category_id' 
-        // );
-            // $query = $em->getResu();
-        // return $em->getResult();
-        // $query = $this->createQueryBuilder('pc')
-        // // ->select('*')
-        // // ->from('product_category')
-        // // ->leftJoin('category', 'id')
-        // ->getQuery()
-        // ->getResult();
-        // // ->leftJoin('category', 'id');
-        // // ->join()
-        // return $query;
-        // dump($this);die();
-
-        
-
-    }
-
     // /**
     //  * @return Product[] Returns an array of Product objects
     //  */
